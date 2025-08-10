ed2bccc93d73fc9c7321fe884c083a1ee8c0e0bd
Ken Kieu <minhcanhdn0704@gmail.com>
2025-08-09 22:54:24 -0700

    fix talkto

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   useScript({
     src: 'https://embed.tawk.to/68982e98a4fc79192a7c0457/1j297c1ae',
     async: true,
-    crossorigin: 'use-credentials',
+    crossorigin: 'anonymous',
     tagPosition: 'head',
   })
 </script>
